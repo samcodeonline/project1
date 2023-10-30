f0f6d4b40494d985de0209f0a032c04bd903cbe1
sarim nadeem <115877996+samcodeonline@users.noreply.github.com>
2023-10-30 14:39:41 +0500

    update

--- controllers/usersControllers.php
+++ controllers/usersControllers.php
@@ -2,8 +2,8 @@
 include "config.php";
 global $db_conn;
 
-if(isset($_POST['name'])){
-    echo "you are going good for sure";
-}
+// if(isset($_POST['name'])){
+//     echo "you are going good for sure";
+// }
 
 ?>
